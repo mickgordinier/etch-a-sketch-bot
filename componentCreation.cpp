96f97c04bbc2a0b1f6c34efdd7932d06294f9d2a
Mick Gordinier <Mickgordinier@gmail.com>
2026-01-06 18:22:59 -0500

    Moving to more efficient BFS for component creation

--- componentCreation.cpp
+++ componentCreation.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <queue>
 
 template <typename T>
 void
@@ -19,40 +20,63 @@ print2DVector(
     std::cout << "\n";
 }
 
+bool inline
+checkNode(
+    const std::vector<std::vector<uint8_t>> &binary_image, 
+    const std::vector<std::vector<int>> &component_tracker,
+    int nodeRow, int nodeCol
+) {
+    if ((nodeRow < 0) || (nodeCol < 0) || (nodeRow >= binary_image.size()) || (nodeCol >= binary_image[0].size())) return false;;
+    if (!binary_image[nodeRow][nodeCol]) return false;
+    if (component_tracker[nodeRow][nodeCol]) return false;
+    return true;
+}
+
 void
 generateNewComponent(
-    const std::vector<std::vector<bool>> &binary_iamge, 
+    const std::vector<std::vector<uint8_t>> &binary_image, 
     std::vector<std::vector<int>> &component_tracker,
     int row, int col, int component_idx 
 ) {
-    if ((row < 0) || (col < 0) || (row >= binary_iamge.size()) || (col >= binary_iamge[0].size())) return;
-    if (!binary_iamge[row][col]) return;
-    if (component_tracker[row][col]) return;
+    if (!checkNode(binary_image, component_tracker, row, col)) return;
 
     component_tracker[row][col] = component_idx;
 
-    for (int i = -1; i <= 1; ++i) {
-        for (int j = -1; j <= 1; ++j) {
-            if (!i && !j) continue;
-            generateNewComponent(binary_iamge, component_tracker, row+i, col+j, component_idx);
+    std::queue<std::pair<int, int>> nodesToCheck;
+    nodesToCheck.push({row, col});
+
+    while(!nodesToCheck.empty()) {
+        std::pair<int, int> currNode = nodesToCheck.front();
+        nodesToCheck.pop();
+
+        int nodeRow = currNode.first;
+        int nodeCol= currNode.second;
+
+        for (int i = -1; i <= 1; ++i) {
+            for (int j = -1; j <= 1; ++j) {
+                if (!i && !j) continue;
+                if (!checkNode(binary_image, component_tracker, nodeRow+i, nodeCol+j)) continue;
+                component_tracker[nodeRow+i][nodeCol+j] = component_idx;
+                nodesToCheck.push({nodeRow+i, nodeCol+j});
+            }
         }
     }
 }
 
 void 
 getComponents(
-    const std::vector<std::vector<bool>> &binary_iamge, 
+    const std::vector<std::vector<uint8_t>> &binary_image, 
     std::vector<std::vector<int>> &component_tracker
 ) {
-    int image_rows = binary_iamge.size();
-    int image_cols = binary_iamge[0].size();
+    int image_rows = binary_image.size();
+    int image_cols = binary_image[0].size();
 
     int component_idx = 1;
 
     for (int row = 0; row < image_rows; ++row) {
         for (int col = 0; col < image_cols; ++col) {
-            if (binary_iamge[row][col] && !component_tracker[row][col]) {
-                generateNewComponent(binary_iamge, component_tracker, row, col, component_idx);
+            if (binary_image[row][col] && !component_tracker[row][col]) {
+                generateNewComponent(binary_image, component_tracker, row, col, component_idx);
                 ++component_idx;
             }
         }
@@ -61,7 +85,7 @@ getComponents(
 
 void
 generateRandomImage(
-    std::vector<std::vector<bool>> &binary_iamge,
+    std::vector<std::vector<uint8_t>> &binary_image,
     int image_rows, int image_cols
 ) {
     std::random_device rd;
@@ -70,7 +94,7 @@ generateRandomImage(
 
     for (int i = 0; i < image_rows; ++i) {
         for (int j = 0; j < image_cols; ++j) {
-            binary_iamge[i][j] = dist(gen);
+            binary_image[i][j] = dist(gen);
         }
     }
 }
@@ -79,18 +103,18 @@ generateRandomImage(
 int 
 main() 
 {
-    // const std::vector<std::vector<bool>> binary_iamge = {
+    // const std::vector<std::vector<uint8_t>> binary_image = {
     //     {1, 0, 0, 0, 0},
     //     {0, 1, 0, 1, 0},
     //     {0, 1, 0, 0, 0},
     //     {0, 1, 1, 1, 1}
     // };
 
-    int image_rows = 10;
-    int image_cols = 10;
+    int image_rows = 10000;
+    int image_cols = 10000;
 
-    std::vector<std::vector<bool>> binary_image(
-        image_rows, std::vector<bool>(image_cols, 0));
+    std::vector<std::vector<uint8_t>> binary_image(
+        image_rows, std::vector<uint8_t>(image_cols, 0));
 
     generateRandomImage(binary_image, image_rows, image_cols);
 
